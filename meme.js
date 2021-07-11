c23f171e52fb1d265c6f93b6fecd747e9df5a876
dcr7978933k <b219021@iiit-bh.ac.in>
2021-07-11 05:33:45 -0700

    first commit

---
+++ meme.js
@@ -0,0 +1,37 @@
+// async function helo(username){
+//     const res=await fetch(`https://meme-api.herokuapp.com/gimme/10`)
+//     //await directly delvers data from url means no then statement
+//     const data=await res.json()
+//     return data
+//    // console.log(res)
+// }
+
+
+//var y=document.querySelector(".but")
+
+ function refreshPage(){
+  window.location.reload();
+} 
+var imgs=document.querySelector(".images")
+var x=async function(){
+
+
+
+  fetch('https://meme-api.herokuapp.com/gimme/8')
+  .then(res=> res.json())
+  .then( data => data.memes.forEach(element => {
+
+    let z=document.createElement("div")
+    z.className="image1"
+    z.innerHTML=`<img src=${element.url}
+     alt=" "/>  <a class="but1" href=${element.url} download>Download</a>` 
+    imgs.appendChild(z)
+      
+  }) )
+  .catch(err=>console.log(err))
+   
+  //  imgs.appendChild(z)
+
+}
+
+x()
